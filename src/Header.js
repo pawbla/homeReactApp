3f0b59cd12bc31f167aeab54d7b29fcda7060af7
Pawel <blachut.pawel@gmail.com>
2019-09-30 22:31:55 +0200

    Used material UI for header

--- src/Header.js
+++ src/Header.js
@@ -1,71 +1,104 @@
 import React from 'react';
 import {Nav} from "./Nav";
 import './header.css';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton, Avatar } from '@material-ui/core';
+import {makeStyles } from '@material-ui/core/styles';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      container: "nav navHide",
-      isOpen: false
-    }
+const useStyles = makeStyles(theme => ({
+  space: {
+    flexGrow: '1'
+  },
+  appBar: {
+    display: 'flex',
+    position: 'fixed',
+    background: '#05A8F5',
+    color: '#f8f8ff',
+    width: '100%',
+    height: '50px',
+    justifyContent: 'center'
+  },
+  title: {
+    display: 'flex',
+  },
+  desktop: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    [theme.breakpoints.down('600')]: {
+      display: 'none',
+    },
+  }, 
+  mobile: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#f8f8ff',
+    margin: '0px',
+    [theme.breakpoints.up('600')]: {
+      display: 'none',
+    },
+  },
+  iconButton: {
+    margin: '10px',
+    height: '70%',
+    color: '#f8f8ff'
+  },
+  avatar: {
+    width: '30px',
+    height: '30px', 
+    margin: '3px',
+    padding: '4px'
   }
+}));
 
-  switchMenu = () => {
-    console.log("Click");
-    this.setState({
-      container:  "nav " + (this.state.isOpen ? " navHide" : "navShow"),
-      isOpen: this.state.isOpen ? false : true,
-    });
-  }
+function Header() {
+  const styles = useStyles();
 
-  render() {
-    return (
-      <header>
-        <div>
-            <MenuIcon menuFunct={this.switchMenu}/>
-            <h2 className="p_title">Home System Service</h2>
-            <div className="avatar-circle">
-                <span className="initials">JD</span>
-            </div>            
-         </div>  
-         <div>
-           <Nav cssClass={this.state.container}/>
-         </div>
-      </header>
-    );
-  }
- }
-
-class MenuIcon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      container: "container",
-      isOpen: false
-    }
-
-  }
-
-  menuAnimation = () => {
-    this.props.menuFunct();
-    this.setState({
-      container:  this.state.isOpen ? "container" : "container change",
-      isOpen: this.state.isOpen ? false : true,
-    });
-    
-  }
-
-  render() {
     return(
-      <div className={this.state.container} onClick={this.menuAnimation}>
-        <div className="bar1"></div>
-        <div className="bar2"></div>
-        <div className="bar3"></div>
+      <div className={styles.space}>
+        <AppBar className={styles.appBar} position="static">
+          <Toolbar>
+            <div className={styles.mobile}>
+              <IconButton
+                edge="start"
+                color="inherit">
+                <MenuIcon />
+              </IconButton>
+            </div>
+            <Typography variant="h5" noWrap className={styles.title}>
+              Home System Service
+            </Typography>
+            <div className={styles.space} />
+            <div className={styles.desktop}>
+              <IconButton className={styles.iconButton}>
+                  <Badge badgeContent={12} color="secondary">
+                    <NotificationsIcon />
+                  </Badge>
+              </IconButton>
+              <IconButton
+                size="small"
+                className={styles.iconButton}>
+                  <Avatar className={styles.avatar}>PB</Avatar>
+              </IconButton>
+            </div>
+            <div className={styles.mobile}>
+              <IconButton
+                edge="end"
+                color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
-      
     );
-  }
- }
+}
 
  export default Header;
