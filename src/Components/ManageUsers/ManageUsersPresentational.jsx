fa8ad795ea2c2628a47c35d892fdd5e6738eb966
Pawel <blachut.pawel@gmail.com>
2020-04-01 22:51:58 +0200

    Manage Users presentational

--- src/Components/ManageUsers/ManageUsersPresentational.jsx
+++ src/Components/ManageUsers/ManageUsersPresentational.jsx
@@ -53,14 +53,6 @@ const useStyles = makeStyles(theme => ({
             borderColor: '#D3D3D3',
         },
     },
-    userDescription: {
-        border: '2px solid #056DF5',
-        borderTopWidth: '0px',
-        margin: '0px 10px',
-        borderBottomLeftRadius: '6px',
-        borderBottomRightRadius: '6px',
-        height: '150px'
-    },
     p_header: {
         boxShadow: '0 0 5px gray',
         padding: '1px 15px', 
@@ -194,13 +186,163 @@ function UserItem(props) {
     )
 }
 
+const userDescStyles = makeStyles(theme => ({
+    userDescription: {
+        border: '2px solid #056DF5',
+        borderTopWidth: '0px',
+        margin: '0px 10px',
+        borderBottomLeftRadius: '6px',
+        borderBottomRightRadius: '6px',
+        height: '150px',
+        '& form': {
+            padding: '8px 10px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'stretch'
+        }
+    },
+    textField: {
+        '& input': {
+        border: '2px solid #056DF5',
+        width: '100%',
+        fontSize: '17px',
+        padding: '4px 7px',
+        margin: '5px 0px',
+        borderRadius: '6px',
+        outline: 'none',
+        background: 'white',
+            '&:focus': {
+                boxShadow: '0 0 13px gray'
+            }
+        },
+    },
+    buttonField: {
+        marginTop: '10px',
+        '& input': {
+            width: '100%',
+            height: '35px',
+            borderRadius: '6px',
+            padding: '2px',
+            margin: '5px 0px',
+            background: '#056DF5',
+            border: '2px solid #056DF5',
+            color: 'white',
+            outline: 'none',
+            fontSize: '18px',
+            '&:active': {
+                background: '#05C5F5',
+                border: '2px solid #F0F8FF',
+                color: '#F0F8FF',
+                boxShadow: '0 0 15px gray',
+            },   
+        }, 
+    }, 
+    rightArea: {
+        width: '230px',
+    },
+    selectField: {
+        '& select': {
+            cursor: 'pointer',
+            border: '2px solid #056DF5',
+            width: '100%',
+            fontSize: '17px',
+            padding: '4px 7px',
+            margin: '5px 0px',
+            borderRadius: '6px',
+            outline: 'none',
+            background: 'white',
+            '&:active': {
+                boxShadow: '0 0 15px gray',
+            },
+        }
+    },
+    checkboxField: {
+        display: 'inline',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        margin: '5px 0px',
+        '& input[type="checkbox"]': {
+            display: 'none',
+        },
+        '& input[type="checkbox"] + span': {
+            height: '30px',
+            width: '30px',   
+            position: 'relative',
+            top: '0px',
+            left: '0px',     
+            border: '2px solid #056DF5',
+            borderRadius: '6px',
+            cursor: 'pointer',  
+        },
+        '& input[type="checkbox"]:checked + span': {
+            background: '#056DF5',
+        },
+        '& label': {
+            marginRight: '10px',
+            fontSize: '20px',
+        }
+    }
+}));
+
 function UserDescription(props) {
 
-    const styles = useStyles();
+    const styles = userDescStyles();
+
+    const [isChecked, setChecked] = useState(false);
+
+    const toogleCheck = () => {
+        setChecked(!isChecked);
+    }
+
+    const disabledColor = (style) => {
+        return props.item.enabled ? style.border : '#4A4949'
+    }
 
     return (
-        <div className={styles.userDescription} style={{borderColor: props.item.enabled ? styles.userDescription.border : '#4A4949'}}>
-            <span>lkajfdhaksdjfhasdkjlfh klsdjfh {props.index}</span>
+        <div className={styles.userDescription} style={{borderColor: disabledColor(styles.userDescription)}}>
+            <form>
+                <div>
+                    <div className={styles.textField}>
+                        <input type="text"
+                                placeholder="Imię"
+                                style={{borderColor: disabledColor(styles.textField)}}>
+                        </input>
+                    </div>
+                    <div className={styles.textField}>
+                        <input type="text"
+                                placeholder="Nazwisko"
+                                style={{borderColor: disabledColor(styles.textField)}}>
+                        </input>
+                    </div>
+                    <div className={styles.textField}>
+                        <input type="text"
+                                placeholder="e-mail"
+                                style={{borderColor: disabledColor(styles.textField)}}>
+                        </input>
+                    </div>
+                </div>
+                <div className={styles.rightArea}>
+                    <div className={styles.selectField}>
+                        <select  style={{borderColor: disabledColor(styles.selectField)}}>
+                            <option className={styles.option}>USER</option>
+                            <option>ADMIN</option>
+                        </select>
+                    </div>
+                    <div className={styles.checkboxField} onClick={toogleCheck}>
+                        <label>Aktywny</label>
+                        <input type="checkbox" checked={isChecked}>
+                        </input>
+                        <span  style={{borderColor: disabledColor(styles.textField)}}></span>
+                    </div>
+                    <div className={styles.buttonField}>
+                        <input 
+                            type='submit'
+                            value="Zatwierdź">
+                        </input>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
